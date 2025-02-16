9bb575c29df141e5f75b7f8271583d0e880e7150
sensorwifi1 <129513098+sensorwifi1@users.noreply.github.com>
2025-02-16 14:24:48 +0100

    Update ota_lora.ino

--- ota_lora.ino
+++ ota_lora.ino
@@ -1,38 +1,52 @@
 #include <WiFi.h>
+#include <WiFiClientSecure.h>  // W ESP32 korzystamy z mbedTLS, nie BearSSL
+#include <HTTPClient.h>        // Klasa HTTPClient jest tu
 #include <FS.h>
 #include <SPIFFS.h>
+#include <TimeLib.h>           // zakładam, że masz tę bibliotekę zainstalowaną
 
+// Jeśli biblioteka ESP_OTA_GitHub używa wewnętrznie BearSSL 
+// (typowe dla wersji ESP8266), może nie działać na ESP32 bez przeróbek:
 #include <ESP_OTA_GitHub.h>
-#include <LoRaNow.h>
-#include <TimeLib.h>
+
+// #include <LoRaNow.h>  // Zakomentowane (pominięty LoraNow)
 
 // WiFi credentials
 #ifndef STASSID
-#define STASSID "Domek"
-#define STAPSK "Otoczyn8"
+#define STASSID "PLAY_Swiatlowod_86C0"
+#define STAPSK "nrZZjrcY2N5V"
 #endif
 
 // GitHub OTA settings
-BearSSL::CertStore certStore;
-#define GHOTA_USER "sensorwifi1"
-#define GHOTA_REPO "lora"
-#define GHOTA_CURRENT_TAG "0.0.0"
-#define GHOTA_BIN_FILE "test.test"
+// BearSSL::CertStore certStore; // <-- Komentarz: w ESP32 to się nie przyda,
+// jeśli biblioteka jest stworzona tylko pod ESP8266
+#define GHOTA_USER              "sensorwifi1"
+#define GHOTA_REPO              "lora"
+#define GHOTA_CURRENT_TAG       "0.0.0"
+#define GHOTA_BIN_FILE          "test.test"
 #define GHOTA_ACCEPT_PRERELEASE 0
 
-// Update time settings
-const int updateHour = 19; // Update at 7:00 PM
-const int updateMinute = 10;
+// Ustal, o której godzinie ma się odbywać update (TimeLib.h)
+const int updateHour   = 19;  // 19:00
+const int updateMinute = 10;  // 19:10
 
 void handle_upgrade() {
-  ESPOTAGitHub ESPOTAGitHub(&certStore, GHOTA_USER, GHOTA_REPO, GHOTA_CURRENT_TAG, GHOTA_BIN_FILE, GHOTA_ACCEPT_PRERELEASE);
+  // Jeśli biblioteka ESP_OTA_GitHub była napisana pod ESP8266, to na ESP32
+  // może wyrzucać błędy. Sprawdź w pliku .h, czy da się usunąć zależność BearSSL.
+  // Niżej i tak pokazuję schemat użycia:
 
+  // Dla ESP8266 byłoby tak:
+  // ESPOTAGitHub ESPOTAGitHub(&certStore, GHOTA_USER, GHOTA_REPO, GHOTA_CURRENT_TAG, GHOTA_BIN_FILE, GHOTA_ACCEPT_PRERELEASE);
+
+  // Dla ESP32 - tymczasowo wywołujemy konstruktor bez 'certStore':
+  ESPOTAGitHub ESPOTAGitHub(NULL, GHOTA_USER, GHOTA_REPO, GHOTA_CURRENT_TAG, GHOTA_BIN_FILE, GHOTA_ACCEPT_PRERELEASE);
+  
   Serial.println("Checking for update...");
   if (ESPOTAGitHub.checkUpgrade()) {
     Serial.print("Upgrade found at: ");
     Serial.println(ESPOTAGitHub.getUpgradeURL());
     if (ESPOTAGitHub.doUpgrade()) {
-      Serial.println("Upgrade complete.");
+      Serial.println("Upgrade complete (device will likely restart).");
     } else {
       Serial.print("Unable to upgrade: ");
       Serial.println(ESPOTAGitHub.getLastError());
@@ -44,19 +58,18 @@ void handle_upgrade() {
 }
 
 void setup() {
-  // Start serial for debugging
   Serial.begin(115200);
+  delay(1000);
 
-  // Start SPIFFS and retrieve certificates
+  // Montujemy SPIFFS (jeśli jest potrzebny do przechowywania certyfikatów)
   if (!SPIFFS.begin(true)) {
-    Serial.println("An Error has occurred while mounting SPIFFS");
-    return;
+    Serial.println("Error mounting SPIFFS");
+    // W kodzie produkcyjnym możesz tu dać while(1);
   }
 
- 
-  }
+  // (W oryginale był certStore.initCertStore(...), ale to dotyczy BearSSL na ESP8266)
 
-  // Connect to WiFi
+  // Łączymy się z WiFi
   Serial.print("Connecting to WiFi... ");
   WiFi.mode(WIFI_STA);
   WiFi.begin(STASSID, STAPSK);
@@ -64,9 +77,10 @@ void setup() {
     delay(1000);
     Serial.print(".");
   }
-  Serial.println("Connected.");
+  Serial.println("\nConnected to WiFi.");
 
-  // LoRaNow setup
+  // (Kod LoRaNow pominięty / zakomentowany)
+  /*
   Serial.println("LoRaNow Simple Node");
   LoRaNow.setFrequencyEU();
   if (!LoRaNow.begin()) {
@@ -76,19 +90,26 @@ void setup() {
   LoRaNow.onMessage(onMessage);
   LoRaNow.onSleep(onSleep);
   LoRaNow.showStatus(Serial);
+  */
 }
 
 void loop() {
-  // Check for updates at 19:00 every day
+  // Sprawdzamy co minutę, czy aktualny czas to 19:10
+  // UWAGA: Jeśli nie masz zsynchronizowanego czasu (NTP), 
+  //       hour() i minute() mogą zwracać błędne wartości.
   if (hour() == updateHour && minute() == updateMinute) {
     handle_upgrade();
-    delay(60000); // Avoid repeated checks in the same minute
+    delay(60000); // odczekaj minutę, żeby nie powtarzać co sekundę
   }
 
-  // LoRaNow loop
+  // (Logika LoRaNow zakomentowana)
+  /*
   LoRaNow.loop();
+  */
 }
 
+// Zakomentuj definicje zdarzeń LoRa, jeśli nie używasz LoRaNow:
+#if 0
 void onMessage(uint8_t *buffer, size_t size) {
   Serial.print("Receive Message: ");
   Serial.write(buffer, size);
@@ -106,3 +127,4 @@ void onSleep() {
   LoRaNow.print(millis());
   LoRaNow.send();
 }
+#endif
